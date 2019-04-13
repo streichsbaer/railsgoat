b1be59504e6d3f22dbb9ea381800116ef02d699e
Stefan Streichsbier <stefan@streichsbier.at>
2019-04-13 10:31:43 +0800

    Create Stripe

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,5 @@
+Rails.configuration.stripe = {
+	:publishable_key => 'pk_live_3cC2OUaORT6Sgitt1ltPeEA1’,
+	:secret_key => 'sk_live_OaASXG1s0xABCp0k5d8AsbaL'
+}
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
