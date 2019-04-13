9804c4ec2ee7ba8e5f251f9df4d27d91c3f7c19d
Stefan Streichsbier <stefan@streichsbier.at>
2019-04-13 10:42:37 +0800

    asdsad

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,5 @@
 Rails.configuration.stripe = {
-	:publishable_key => 'pk_live_3cC2OUaORT6Sgitt1ltPeEA1’,
-	:secret_key => 'sk_live_OaASXG1s0xABCp0k5d8AsbaL'
+	:publishable_key => ENV['PUBLISHABLE_KEY'],
+	:secret_key => ENV['SECRET_KEY']
 }
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
